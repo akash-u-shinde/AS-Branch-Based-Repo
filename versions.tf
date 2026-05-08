2237339605b929ef8ccc905d17e6c80ff44be808
akash-u-shinde <akash.shinde1808@gmail.com>
2026-05-08 11:17:12 +0530

    added some other files

---
+++ versions.tf
@@ -0,0 +1,14 @@
+terraform {
+    required_provider {
+        azurerm = {
+            source="hashicorp/azurerm"
+            version="~>4.0"
+        }
+    }
+
+    backend "azurerm" {
+        # Details configured by Github Action
+        use_azuread_auth= true
+        use_oidc= true 
+    }
+}
